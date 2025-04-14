f293cd1a20eb27cce4e67ae199016e5f76f7cc6a
MohitR1999 <mohitr1999@gmail.com>
2025-04-14 23:47:06 +0530

    Modified table to show more details about points table

--- ipl-info-ui/next.config.ts
+++ ipl-info-ui/next.config.ts
@@ -3,6 +3,9 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL('https://scores.iplt20.com/ipl/teamlogos/**')],
+  },
 };
 
 export default withFlowbiteReact(nextConfig);
--- ipl-info-ui/src/app/points/page.tsx
+++ ipl-info-ui/src/app/points/page.tsx
@@ -1,13 +1,100 @@
 'use client'
 
-import { Card } from "flowbite-react";
+import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface TableItemProps {
+    name: string;
+    position : string;
+    image : string;
+    points : string;
+}
+
+const TableItem = ({
+    name,
+    position,
+    image,
+    points
+}: TableItemProps) => {
+    return (
+        <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {position}
+            </TableCell>
+            <TableCell>
+                <>
+                    {name}
+                </>
+            </TableCell>
+            <TableCell>
+                {points}
+            </TableCell>
+        </TableRow>
+    );
+}
 
 const Points = () => {
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchPointsTable = async () => {
+            const response = await fetch(`http://${process.env.NEXT_PUBLIC_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api/points`);
+            const pointsData = await response.json();
+            console.log(pointsData);
+            setLoading(false);
+            setData(pointsData['data']['points']);
+        }
+
+        fetchPointsTable()
+            .catch(err => {
+                console.log(err);
+                setLoading(false);
+                setData([]);
+            })
+            ;
+    }, []);
+
     return (
-        <div>
-            Points table will be displayed here
-        </div>
+        <>
+            {
+                loading ?
+                    <div>
+                        Loading...
+                    </div>
+                    :
+                    <div className="flex flex-col justify-center items-center">
+                        <h1 className="text-lg m-8">Points table</h1>
+                        <div className="overflow-x-auto max-w-full flex-1">
+                            <Table className="min-w-full">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableHeadCell>Position</TableHeadCell>
+                                        <TableHeadCell>Team</TableHeadCell>
+                                        <TableHeadCell>Points</TableHeadCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody className="divide-y">
+                                    {(data || []).map((item: any) => {
+                                        return <TableItem
+                                            key={item.TeamID}
+                                            name={item.TeamName}
+                                            position={item.OrderNo}
+                                            image={item.TeamLogo}
+                                            points={item.Points}
+                                        />
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </div>
+                    </div>
+            }
+        </>
+
     );
 }
 
 export default Points;
+

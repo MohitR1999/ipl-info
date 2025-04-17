20af1894d480706ea50ddea93153c21129497bd2
MohitR1999 <mohitr1999@gmail.com>
2025-04-17 12:19:33 +0530

    Added drawer navigation for proper UX

--- ipl-info-ui/src/app/nav/Nav.tsx
+++ ipl-info-ui/src/app/nav/Nav.tsx
@@ -1,52 +1,112 @@
 'use client'
-import { Navbar, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { Navbar } from "flowbite-react";
 import { useRouter, usePathname } from "next/navigation";
 
+import {
+    Button,
+    Drawer,
+    DrawerHeader,
+    DrawerItems,
+    Sidebar,
+    SidebarItem,
+    SidebarItemGroup,
+    SidebarItems,
+} from "flowbite-react";
+import { useState } from "react";
+import {
+    HiMenu,
+    HiHome,
+    HiTable,
+    HiCalendar
+} from "react-icons/hi";
+
+
 const Nav = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
     const router = useRouter();
     const pathName = usePathname();
     const navItems = [
         {
-            label : 'Home',
-            href : '/'
+            label: 'Home',
+            href: '/',
+            icon : () => <HiHome />
         },
-
         {
-            label : 'Points table',
-            href : '/points'
+            label: 'Points table',
+            href: '/points',
+            icon : () => <HiTable />
         },
-
         {
-            label : 'Match Schedule',
-            href : '/schedule'
+            label: 'Match Schedule',
+            href: '/schedule',
+            icon : () => <HiCalendar />
         }
     ]
-  
+
+    const handleClose = () => setIsOpen(false);
+
     return (
-    <Navbar fluid rounded>
-      <NavbarToggle />
-      <NavbarCollapse>
-        {
-            navItems.map(item => {
-                const isActive = pathName === item.href;
-                
-                return (
-                <NavbarLink 
-                    key={item.href}
-                    href="#"
-                    active={isActive}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        router.push(item.href);
-                    }}
-                >
-                    {item.label}
-                </NavbarLink>)
-            })
-        }
-      </NavbarCollapse>
-    </Navbar>
-  );
+        <>
+            <Navbar fluid style={{
+                backgroundColor : 'rgba(0, 0, 0, 0)'
+            }}>
+                <Button color="dark" style={{
+                    padding: 10,
+                    borderRadius : 300,
+                    backgroundColor : 'rgba(0, 0, 0, 0)'
+                }} onClick={(e) => {
+                    e.preventDefault();
+                    setIsOpen(!isOpen);
+                }}>
+                    <HiMenu className="h-6 w-6" />
+                </Button>
+                <Drawer open={isOpen} onClose={handleClose}>
+                    <DrawerHeader title="MENU" titleIcon={() => <></>} />
+                    <DrawerItems>
+                        <Sidebar
+                            aria-label="Sidebar with multi-level dropdown example"
+                            className="[&>div]:bg-transparent [&>div]:p-0"
+                        >
+                            <div className="flex h-full flex-col justify-between py-2">
+                                <div>
+                                    <SidebarItems>
+                                        <SidebarItemGroup>
+                                            {
+                                                navItems.map(item => {
+                                                    const isActive = pathName === item.href;
+                                                    
+                                                    return (
+                                                        <SidebarItem
+                                                            key={item.href}
+                                                            href="#"
+                                                            active={isActive}
+                                                            icon={item.icon}
+                                                            onClick={(e) => {
+                                                                e.preventDefault();
+                                                                router.push(item.href);
+                                                                setIsOpen(false);
+                                                            }}
+                                                        >
+                                                            {item.label}
+                                                        </SidebarItem>
+                                                    );
+                                                })
+                                            }
+                                        </SidebarItemGroup>
+                                    </SidebarItems>
+                                </div>
+                            </div>
+                        </Sidebar>
+                    </DrawerItems>
+                </Drawer>
+            </Navbar>
+
+        </>
+    );
 }
 
 export default Nav;
+
+
+

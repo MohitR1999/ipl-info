2d1081c80e8c3ac4414339bb191aedf295d4a5eb
MohitR1999 <mohitr1999@gmail.com>
2025-04-19 19:53:42 +0530

    modified socket URL to use new environment variable

--- ipl-info-ui/src/app/utils/socket.js
+++ ipl-info-ui/src/app/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const URL = `http://${process.env.NEXT_PUBLIC_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/`
+const URL = `${process.env.NEXT_PUBLIC_URL}`
 
 export const socket = io(URL, {
     autoConnect : false

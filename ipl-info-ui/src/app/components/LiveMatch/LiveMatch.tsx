62094b628c23df15f90f18fee385664cf116e373
MohitR1999 <mohitr1999@gmail.com>
2025-04-20 20:31:31 +0530

    added live score box

--- ipl-info-ui/src/app/components/LiveMatch/LiveMatch.tsx
+++ ipl-info-ui/src/app/components/LiveMatch/LiveMatch.tsx
@@ -3,14 +3,79 @@
 import {
     Timeline,
     TabItem,
-    Tabs
+    Tabs,
+    Card
 } from "flowbite-react";
 import { useEffect, useState } from "react";
-
+import Image from "next/image";
 import Loader from "../Loader/Loader";
 import OverDetail from "../OverDetail/OverDetail";
 import { socket } from '@/app/utils/socket';
 
+const StatsCard = (props: any) => {
+    const IMG_SIZE = 100
+
+    const homeTeamStats = props.firstInnings.includes(props.homeTeamName) ? props.firstSummary : props.secondSummary;
+    const awayTeamStats = props.firstInnings.includes(props.awayTeamName) ? props.firstSummary : props.secondSummary;
+
+    console.log({
+        homeTeamStats,
+        awayTeamStats
+    });
+
+    return (
+        <div className='flex p-5 flex-col justify-center items-center'>
+            <Card className="flex-1 max-w-md w-full">
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {props.matchName}
+                </h5>
+
+                <div className='flex justify-between items-center'>
+                    <div className='flex flex-col justify-between items-center'>
+                        {
+                            props.homeTeamLogo && props.homeTeamName
+                                ?
+                                <Image
+                                    src={props.homeTeamLogo}
+                                    width={IMG_SIZE}
+                                    height={IMG_SIZE}
+                                    alt={props.homeTeamName} />
+                                : <></>
+                        }
+                        <div className='text-gray-900 dark:text-gray-100'>
+                            {homeTeamStats ? homeTeamStats : "-/-"}
+                        </div>
+                    </div>
+                    <h6 className="text-xl text-gray-900 dark:text-gray-400">
+                        vs
+                    </h6>
+                    <div className='flex flex-col justify-between items-center'>
+                        {
+                            props.awayTeamLogo && props.awayTeamName
+                                ?
+                                <Image
+                                    src={props.awayTeamLogo}
+                                    width={IMG_SIZE}
+                                    height={IMG_SIZE}
+                                    alt={props.awayTeamName} />
+                                : <></>
+                        }
+                        <div className='text-gray-900 dark:text-gray-100'>
+                            {awayTeamStats ? awayTeamStats : " "}
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex items-center'>
+                    <p className="text-gray-700 dark:text-gray-400">
+                        {props.tossDetails}
+                    </p>
+                </div>
+
+            </Card>
+        </div>
+    );
+}
 
 const LiveMatch = ({
     match
@@ -19,15 +84,55 @@ const LiveMatch = ({
         match: string
     }) => {
 
-    const [innings1, setInnings1] = useState([]);
-    const [innings2, setInnings2] = useState([]);
+    const [innings1, setInnings1] = useState<any>([]);
+    const [innings2, setInnings2] = useState<any>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [chasingText, setChasingText] = useState("");
+    const [tossDetails, setTossDetails] = useState("");
+    const [matchName, setMatchName] = useState("");
+    const [homeTeamLogo, setHomeTeamLogo] = useState("");
+    const [awayTeamLogo, setAwayTeamLogo] = useState("");
+    const [homeTeamName, setHomeTeamName] = useState("");
+    const [awayTeamName, setAwayTeamName] = useState("");
+    const [firstSummary, setFirstSummary] = useState("");
+    const [secondSummary, setSecondSummary] = useState("");
+    const [firstInnings, setFirstInnings] = useState("");
+    const [secondInnings, setSecondInnings] = useState("");
 
     const handleLiveScoreUpdate = (msg: any) => {
         console.log(msg);
         setChasingText(msg['ChasingText'])
+        setTossDetails(msg['TossDetails'])
+        setFirstInnings(msg['1InningsName']);
+        setSecondInnings(msg['2InningsName']);
+        setFirstSummary(msg['1Summary']);
+        setSecondSummary(msg['2Summary']);
     }
+
+    const handleLiveCommentaryUpdate = (msg: any) => {
+        switch (msg.InningsNo) {
+            case 1: {
+                setInnings1((prevInnings1 : any) => {
+                    if (prevInnings1.length === 0 || msg.BallUniqueID !== prevInnings1[0].BallUniqueID) {
+                        return [msg, ...prevInnings1];
+                    }
+                    return prevInnings1;
+                });
+                break;
+            }
+            case 2: {
+                setInnings2((prevInnings2 : any) => {
+                    if (prevInnings2.length === 0 || msg.BallUniqueID !== prevInnings2[0].BallUniqueID) {
+                        return [msg, ...prevInnings2];
+                    }
+                    return prevInnings2;
+                });
+                break;
+            }
+            default:
+                break;
+        }
+    };
 
     useEffect(() => {
         setIsLoading(true);
@@ -51,6 +156,16 @@ const LiveMatch = ({
                 const result = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/summary?matchId=${match}`);
                 const data = await result.json();
                 setChasingText(data['ChasingText']);
+                setTossDetails(data['TossDetails']);
+                setMatchName(data['MatchName']);
+                setHomeTeamLogo(data['HomeTeamLogo']);
+                setAwayTeamLogo(data['AwayTeamLogo']);
+                setHomeTeamName(data['HomeTeamName']);
+                setAwayTeamName(data['AwayTeamName']);
+                setFirstInnings(data['1InningsName']);
+                setSecondInnings(data['2InningsName']);
+                setFirstSummary(data['1Summary']);
+                setSecondSummary(data['2Summary']);
             } catch (err) {
                 console.log(err);
             }
@@ -78,13 +193,15 @@ const LiveMatch = ({
     useEffect(() => {
         socket.connect();
         socket.on('live-score', handleLiveScoreUpdate);
+        socket.on('live-commentary', handleLiveCommentaryUpdate);
         const interval = setInterval(() => {
-            console.log('Going to fetch Live scores');
             socket.emit('get-score', { match: match });
+            socket.emit('get-commentary', { match: match });
         }, 10 * 1000);
 
         return () => {
             socket.off('live-score', handleLiveScoreUpdate);
+            socket.off('live-commentary', handleLiveCommentaryUpdate);
             socket.disconnect();
             clearInterval(interval);
         }
@@ -97,7 +214,18 @@ const LiveMatch = ({
                 isLoading ? <Loader /> :
                     <div className="flex flex-col">
                         <div>
-                            {chasingText}
+                            <StatsCard
+                                tossDetails={tossDetails}
+                                matchName={matchName}
+                                homeTeamLogo={homeTeamLogo}
+                                awayTeamLogo={awayTeamLogo}
+                                homeTeamName={homeTeamName}
+                                awayTeamName={awayTeamName}
+                                firstInnings={firstInnings}
+                                firstSummary={firstSummary}
+                                secondInnings={secondInnings}
+                                secondSummary={secondSummary}
+                            />
                         </div>
 
                         <Tabs aria-label="Pills" variant="fullWidth">

58d7484a6070191f47c602e79df7c22dbdf72e68
MohitR1999 <mohitr1999@gmail.com>
2025-04-20 17:51:09 +0530

    added handling for live match commentary and navigation

--- ipl-info-ui/src/app/page.tsx
+++ ipl-info-ui/src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { TabItem, Tabs, Card, Button } from 'flowbite-react';
+import { TabItem, Tabs } from 'flowbite-react';
 import { useState, useEffect } from 'react';
-import { HiChartBar, HiCalendar, HiClock, HiLocationMarker } from "react-icons/hi";
-import { MdDashboard, MdSportsCricket } from "react-icons/md";
-import Image from "next/image";
-import UpcomingMatch from './components/UpcomingMatch/UpcomingMatch';
-import { socket } from './utils/socket';
+import { HiChartBar, HiCalendar } from "react-icons/hi";
+import { MdSportsCricket } from "react-icons/md";
+import { useRouter } from 'next/navigation'
+import MatchCard from './components/MatchCard/MatchCard';
+import Loader from './components/Loader/Loader';
 
 
 interface MatchDetailsProps {
@@ -17,7 +17,7 @@ interface MatchDetailsProps {
 const NoLiveMatchPlaceHolder = () => {
   return (
     <div className='p-4 m-4 flex flex-col justify-center items-center'>
-      <MdSportsCricket className='w-25 h-25 mb-10 text-gray-400'/>
+      <MdSportsCricket className='w-25 h-25 mb-10 text-gray-400' />
       <div className='text-3xl text-gray-400'>
         No live match for now
       </div>
@@ -25,43 +25,23 @@ const NoLiveMatchPlaceHolder = () => {
   );
 }
 
-const MatchDetails = (props: MatchDetailsProps) => {
-  const date = new Date(props.upcomingMatch.MATCH_COMMENCE_START_DATE);
-  const isMatchLive = props.liveMatch.length > 0;
-
-  return (
-    <div>
-      <Tabs aria-label="Default tabs" variant="fullWidth">
-        <TabItem active title="Live Match" icon={HiChartBar}>
-          {
-            isMatchLive ? <div>Live Match details</div> : <NoLiveMatchPlaceHolder />
-          }
-        </TabItem>
-        <TabItem  title="Upcoming Match" icon={HiCalendar}>
-          <UpcomingMatch
-            upcomingMatch={props.upcomingMatch}
-            date={date} />
-        </TabItem>
-      </Tabs>
-    </div>
-  );
-}
 
 export default function Home() {
   const [data, setData] = useState([]);
-  const [upcomingMatch, setUpcomingMatch] = useState({});
+  const [upcomingMatch, setUpcomingMatch] = useState<any>({});
   const [liveMatch, setLiveMatch] = useState([]);
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   const handleConnect = () => {
     console.log('Socket connected!');
   }
 
-  const handleDisconnect = (msg : any) => {
+  const handleDisconnect = (msg: any) => {
     console.log('Socket disconnected');
   }
 
-  const handleLiveMatch = (msg : any) => {
+  const handleLiveMatch = (msg: any) => {
     // take the data and set in the live match details
     console.log(msg);
   }
@@ -79,10 +59,10 @@ export default function Home() {
         setUpcomingMatch({});
       }
     }
-    
+
     const fetchLiveMatchDetails = async () => {
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/live`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/live-matches`);
         const matchData = await response.json();
         setLiveMatch(matchData);
         setLoading(false);
@@ -94,38 +74,60 @@ export default function Home() {
     }
 
     fetchUpcomingMatchDetails()
-    .catch (err => {
-      console.log(err);
-    })
-    .then (() => {
-     return fetchLiveMatchDetails();
-    })
+      .catch(err => {
+        console.log(err);
+      })
+      .then(() => {
+        return fetchLiveMatchDetails();
+      })
       .catch(err => {
         console.log(err);
         setLoading(false);
       });
   }, []);
 
-  useEffect(() => {
-    socket.connect();
-    socket.on('connect', handleConnect);
-    socket.on('disconnect', handleDisconnect);
-    socket.on('live-match-update', handleLiveMatch);
-
-    return () => {
-      socket.off('connect', handleConnect);
-      socket.off('disconnect', handleDisconnect);
-      socket.off('live-match-update', handleLiveMatch);
-      socket.disconnect();
-    }
-  }, []);
-
-
   return (
-    loading ? <div>Loading...</div>
-      :
-      <MatchDetails
-        liveMatch={liveMatch}
-        upcomingMatch={upcomingMatch} />
+    <>
+      {
+        loading ? <Loader />
+          :
+          <Tabs aria-label="Default tabs" variant="fullWidth">
+            <TabItem active title="Live" icon={HiChartBar}>
+              <div>
+                {
+                  liveMatch.length > 0 ?
+                  liveMatch.map((m: any) => {
+                    return <MatchCard
+                      {...m}
+                      date={new Date(m.MATCH_COMMENCE_START_DATE)}
+                      key={m.MatchID}
+                      handleClick={e => {
+                        e.preventDefault();
+                        router.push(`/${m.MatchID}`)
+                      }}
+                      actionText='Go to commentary'
+                    />
+                  })
+                  :
+                  <NoLiveMatchPlaceHolder />
+                }
+              </div>
+            </TabItem>
+            <TabItem title="Upcoming" icon={HiCalendar}>
+              <MatchCard
+                {...upcomingMatch}
+                date={new Date(upcomingMatch.MATCH_COMMENCE_START_DATE)}
+                handleClick={(e) => {
+                  e.preventDefault();
+                  window.location.href = upcomingMatch.FBURL
+                }}
+                actionText="Book Tickets"
+              />
+            </TabItem>
+          </Tabs>
+      }
+    </>
+
+
   )
 }
